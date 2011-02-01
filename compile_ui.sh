c4435b1f95d983ccb816400deb3c992b6f59e418
Sebastiaan Mathot <s.mathot@cogsci.nl>
2011-02-01 16:52:26 +0100

    adding some more stuff

---
+++ compile_ui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+pyuic4 resources/opensesame.ui > libqtopensesame/opensesame_ui.py
+pyuic4 resources/general_widget.ui > libqtopensesame/general_widget_ui.py
+pyuic4 resources/sketchpad_widget.ui > libqtopensesame/sketchpad_widget_ui.py
+pyuic4 resources/sampler_widget.ui > libqtopensesame/sampler_widget_ui.py
+pyuic4 resources/synth_widget.ui > libqtopensesame/synth_widget_ui.py
+pyuic4 resources/pool_widget.ui > libqtopensesame/pool_widget_ui.py
+pyuic4 resources/notification_dialog.ui > libqtopensesame/notification_dialog_ui.py
+pyuic4 resources/update_dialog.ui > libqtopensesame/update_dialog_ui.py
+pyuic4 resources/tip_dialog.ui > libqtopensesame/tip_dialog_ui.py
+pyuic4 resources/loop_wizard_dialog.ui > libqtopensesame/loop_wizard_dialog_ui.py
+pyuic4 resources/gabor_dialog.ui > libqtopensesame/gabor_dialog_ui.py
+pyuic4 resources/noise_patch_dialog.ui > libqtopensesame/noise_patch_dialog_ui.py
+pyuic4 resources/new_loop_sequence.ui > libqtopensesame/new_loop_sequence_ui.py
+pyrcc4 resources/icons.qrc > libqtopensesame/icons_rc.py
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import glob
+import os
+import os.path
+
+# Not all plugins are included
+included_plugins = ["advanced_delay", "external_script", "fixation_dot", "text_display", "text_input"]
+
+def plugins():
+    
+    """
+    Create a list of all plugins that need to be included in the release
+    """
+    
+	global included_plugins
+
+	l = []
+	for plugin in os.listdir("plugins"):
+	
+		if plugin in included_plugins:
+			l.append( ("/usr/share/opensesame/plugins/%s" % plugin, glob.glob("plugins/%s/*" % plugin)) )
+			
+	return l				
+
+setup(name="opensesame",
+
+	version = "0.23-pre1",
+	description = "A graphical experiment builder for the social sciences",
+	author = "Sebastiaan Mathot",
+	author_email = "s.mathot@cogsci.nl",
+	url = "http://www.cogsci.nl",
+	scripts = ["opensesame"],	      
+	packages = ["openexp", "libopensesame", "libqtopensesame"],
+	package_dir = {"openexp" : "openexp", "libopensesame" : "libopensesame", "libqtopensesame" : "libqtopensesame"},
+	data_files=[
+		("/usr/share/opensesame", ["COPYING"]),
+		("/usr/share/applications", ["data/opensesame.desktop"]),
+		("/usr/share/opensesame/resources", glob.glob("resources/*.png") + glob.glob("resources/*.opensesame") + glob.glob("resources/*.ttf") + ["resources/README", "resources/tips.txt"]),
+		("/usr/share/opensesame/help", glob.glob("help/*.html")),
+		("/usr/share/opensesame/sounds", glob.glob("sounds/*")),
+		("/usr/share/opensesame/examples", glob.glob("examples/*")),
+		] + plugins()
+	)
+	
+	
