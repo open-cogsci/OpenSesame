a91a6dd3f973a31e6d7d4c920065eb0900b19163
Sebastiaan Mathot <s.mathot@cogsci.nl>
2011-10-04 12:14:53 +0200

    Version bump to 0.25-pre3

--- libopensesame/misc.py
+++ libopensesame/misc.py
@@ -27,7 +27,7 @@ import libqtopensesame
 import libopensesame.experiment
 from Tkinter import *
 
-version = "0.25-pre3"
+version = "0.25-pre4"
 codename = "Dashy Darwin"
 
 def change_working_dir():
