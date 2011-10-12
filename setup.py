f2e90ceb2cde8b34d6380cd908524ffc0a0d0fdb
Sebastiaan Mathot <s.mathot@cogsci.nl>
2011-10-12 14:50:04 +0200

    Improved setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import libopensesame.misc
 
 # Not all plugins are included
 included_plugins = ["advanced_delay", "external_script", "fixation_dot", \
-	"text_display", "text_input", "notepad", "reset_feedback"]
+	"text_display", "text_input", "notepad", "reset_feedback", "srbox"]
 
 def plugins():
 
@@ -21,13 +21,11 @@ def plugins():
 	"""
 	
 	global included_plugins
-
 	l = []
 	for plugin in os.listdir("plugins"):
-
 		if plugin in included_plugins:
-			l.append( ("/usr/share/opensesame/plugins/%s" % plugin, glob.glob("plugins/%s/*" % plugin)) )
-
+			l.append( ("/usr/share/opensesame/plugins/%s" % plugin, \
+				glob.glob("plugins/%s/*" % plugin)) )
 	return l
 
 setup(name="opensesame",
@@ -36,10 +34,13 @@ setup(name="opensesame",
 	description = "A graphical experiment builder for the social sciences",
 	author = "Sebastiaan Mathot",
 	author_email = "s.mathot@cogsci.nl",
-	url = "http://www.cogsci.nl",
+	url = "http://www.cogsci.nl/",
 	scripts = ["opensesame", "opensesamerun"],
-	packages = ["openexp", "openexp._canvas", "openexp._keyboard", "openexp._mouse", "openexp._sampler", "openexp._synth", "libopensesame", "libqtopensesame"],
-	package_dir = {"openexp" : "openexp", "libopensesame" : "libopensesame", "libqtopensesame" : "libqtopensesame"},
+	packages = ["openexp", "openexp._canvas", "openexp._keyboard", \
+		"openexp._mouse", "openexp._sampler", "openexp._synth", "libopensesame", \
+		"libqtopensesame"],
+	package_dir = {"openexp" : "openexp", "libopensesame" : "libopensesame", \
+		"libqtopensesame" : "libqtopensesame"},
 	data_files=[
 		("/usr/share/opensesame", ["COPYING"]),
 		("/usr/share/applications", ["data/opensesame.desktop"]),
@@ -50,7 +51,9 @@ setup(name="opensesame",
 			+ ["resources/tips.txt"]),
 		("/usr/share/opensesame/help", glob.glob("help/*.html")),
 		("/usr/share/opensesame/sounds", glob.glob("sounds/*")),
-		("/usr/share/opensesame/examples", glob.glob("examples/*")),
+		("/usr/share/opensesame/examples", \
+			glob.glob("examples/*.opensesame") + \
+			glob.glob("examples/*.opensesame.tar.gz")),
 		] + plugins()
 	)
 
