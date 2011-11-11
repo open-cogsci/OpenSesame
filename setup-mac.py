2662e5cdd947aacd25a5b95112289f3ceb39fe57
Sebastiaan Mathot <s.mathot@cogsci.nl>
2011-11-11 13:37:59 +0100

    Updated setup-mac.py to include cv and exclude unwanted plugins

--- setup-mac.py
+++ setup-mac.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
 """
+This file is part of OpenSesame.
+
+OpenSesame is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenSesame is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
+
 This scripts builds OpenSesame as a standalone application for Mac OS.
 
 Usage:
@@ -41,7 +56,7 @@ setup(
     data_files = ['opensesame.py'],
     options = {'py2app' : {'argv_emulation': False, 
 		'includes' : ['sip', 'PyQt4.QtGui', 'PyQt4.QtCore', 'pygame', 'numpy', \
-			'serial', 'openexp', 'libqtopensesame', 'libopensesame' \
+			'serial', 'openexp', 'libqtopensesame', 'libopensesame', 'cv' \
 			],
 		'resources' : ['qt_menu.nib', 'resources', 'examples', 'sounds', \
 			'plugins', 'help', 'data'
@@ -52,6 +67,10 @@ setup(
 
 # Clean up qt_menu.nib
 shutil.rmtree("qt_menu.nib")
+
+# Remove unwanted plugins from build
+shutil.rmtree("dist/opensesame.app/Contents/Resources/plugins/pp_io")
+shutil.rmtree("dist/opensesame.app/Contents/Resources/plugins/port_reader")
 
 # Remove opensesame.py
 try:
