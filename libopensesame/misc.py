3b8489ed3039b98838f43aa39876fc4f34d6850f
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-03-29 16:04:39 +0200

    Update to 0.26-pre9

--- libopensesame/misc.py
+++ libopensesame/misc.py
@@ -22,7 +22,7 @@ import os
 import os.path
 import sys
 
-version = "0.26-pre8"
+version = "0.26-pre9"
 codename = "Earnest Einstein"
 
 use_global_resources = "--no-global-resources" not in sys.argv
