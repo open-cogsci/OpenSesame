e3ff6b6fba3e761676454de5e38a69dfa1fb2f77
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-03-29 13:04:59 +0200

    Bump to 0.26-pre8

--- libopensesame/misc.py
+++ libopensesame/misc.py
@@ -22,7 +22,7 @@ import os
 import os.path
 import sys
 
-version = "0.26-pre7"
+version = "0.26-pre8"
 codename = "Earnest Einstein"
 
 use_global_resources = "--no-global-resources" not in sys.argv
