d0c5bc57a1af00337145f86ef43df19a51621d24
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-06-22 17:14:29 +0200

    Cleanup notepad plugin

--- plugins/notepad/notepad.py
+++ plugins/notepad/notepad.py
@@ -23,111 +23,80 @@ from PyQt4 import QtGui, QtCore
 
 class notepad(item.item):
 
-	"""
-	This class (the class with the same name as the module)
-	handles the basic functionality of the item. It does
-	not deal with GUI stuff.
-	"""
+	"""Notepad plug-in"""
 
-	def __init__(self, name, experiment, string = None):
+	def __init__(self, name, experiment, string=None):
 	
 		"""
 		Constructor
+
+		Arguments:
+		name -- the name of the item
+		experiment -- the experiment
+
+		Keyword arguments:
+		string -- an item definition string (default = None)
 		"""
 		
-		# The item_typeshould match the name of the module
 		self.item_type = "notepad"
 		self.note = "Type your note here"
-		
-		# Provide a short accurate description of the items functionality
-		self.description = "A simple notepad to document your experiment. This plug-in does nothing."
-				
-		# The parent handles the rest of the contruction
+		self.description = \
+			"A simple notepad to document your experiment. This plug-in does nothing."
 		item.item.__init__(self, name, experiment, string)					
 					
 class qtnotepad(notepad, qtplugin.qtplugin):
 
-	"""
-	This class (the class named qt[name of module] handles
-	the GUI part of the plugin. For more information about
-	GUI programming using PyQt4, see:
-	<http://www.riverbankcomputing.co.uk/static/Docs/PyQt4/html/classes.html>
-	"""
+	"""Notepad plug-in GUI"""
 
-	def __init__(self, name, experiment, string = None):
+	def __init__(self, name, experiment, string=None):
 	
 		"""
 		Constructor
+
+		Arguments:
+		name -- the name of the item
+		experiment -- the experiment
+
+		Keyword arguments:
+		string -- an item definition string (default = None)
 		"""
 		
-		# Pass the word on to the parents		
 		notepad.__init__(self, name, experiment, string)		
 		qtplugin.qtplugin.__init__(self, __file__)	
 		
 	def init_edit_widget(self):
 	
-		"""
-		This function creates the controls for the edit
-		widget.
-		"""
+		"""Initialize controls"""
 		
-		# Lock the widget until we're doing creating it
-		self.lock = True
-		
-		# Pass the word on to the parent		
-		qtplugin.qtplugin.init_edit_widget(self, False)
-		
-		# Create the controls
-		# 
-		# A number of convenience functions are available which 
-		# automatically create controls, which are also automatically
-		# updated and applied. If you set the varname to None, the
-		# controls will be created, but not automatically updated
-		# and applied.
-		#
-		# qtplugin.add_combobox_control(varname, label, list_of_options)
-		# - creates a QComboBox
-		# qtplugin.add_line_edit_control(varname, label)
-		# - creates a QLineEdit		
-		# qtplugin.add_spinbox_control(varname, label, min, max, suffix = suffix, prefix = prefix)
-		
-		self.add_editor_control("note", "Note", tooltip = "Type your note here")
-				
-		# Unlock
+		self.lock = True		
+		qtplugin.qtplugin.init_edit_widget(self, False)		
+		self.add_editor_control("note", "Note", tooltip = "Type your note here")				
 		self.lock = True		
 		
 	def apply_edit_changes(self):
 	
 		"""
-		Set the variables based on the controls
+		Apply controls
+		
+		Returns:
+		True on success, False on failure		
 		"""
 		
-		# Abort if the parent reports failure of if the controls are locked
 		if not qtplugin.qtplugin.apply_edit_changes(self, False) or self.lock:
 			return False
-				
-		# Refresh the main window, so that changes become visible everywhere
 		self.experiment.main_window.refresh(self.name)		
-		
-		# Report success
 		return True
 
 	def edit_widget(self):
 	
 		"""
-		Set the controls based on the variables
+		Update the controls
+		
+		Returns:
+		The QWidget containing the controls
 		"""
 		
-		# Lock the controls, otherwise a recursive loop might aris
-		# in which updating the controls causes the variables to be
-		# updated, which causes the controls to be updated, etc...
 		self.lock = True
-		
-		# Let the parent handle everything
 		qtplugin.qtplugin.edit_widget(self)				
-		
-		# Unlock
 		self.lock = False
-		
-		# Return the _edit_widget
 		return self._edit_widget
