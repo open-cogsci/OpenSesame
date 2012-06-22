4b2d0f29d489609638dbbcdf4adf1a7c475a04ed
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-06-22 17:39:04 +0200

    Cleanup inline_script GUI code

--- libqtopensesame/items/inline_script.py
+++ libqtopensesame/items/inline_script.py
@@ -41,7 +41,8 @@ class inline_script(libopensesame.inline_script.inline_script, qtitem.qtitem):
 		string -- definition string
 		"""
 
-		libopensesame.inline_script.inline_script.__init__(self, name, experiment, string)
+		libopensesame.inline_script.inline_script.__init__(self, name, \
+			experiment, string)
 		qtitem.qtitem.__init__(self)
 		self.lock = False
 		self._var_info = None
@@ -80,45 +81,46 @@ class inline_script(libopensesame.inline_script.inline_script, qtitem.qtitem):
 		qtitem.qtitem.init_edit_widget(self, False)
 
 		tabwidget_script = QtGui.QTabWidget(self._edit_widget)
-		py_ver = "Python %d.%d.%d" % (sys.version_info[0], sys.version_info[1], sys.version_info[2])
-
-		self.textedit_prepare = inline_editor.inline_editor(self.experiment, notification=py_ver, syntax="python")
+		py_ver = "Python %d.%d.%d" % (sys.version_info[0], \
+			sys.version_info[1], sys.version_info[2])
+			
+		# Construct prepare editor
+		self.textedit_prepare = inline_editor.inline_editor(self.experiment, \
+			notification=py_ver, syntax="python")
 		self.textedit_prepare.apply.clicked.connect(self.apply_edit_changes)
-		QtCore.QObject.connect(self.textedit_prepare.edit, QtCore.SIGNAL("focusLost"), self.apply_edit_changes)
-
+		QtCore.QObject.connect(self.textedit_prepare.edit, QtCore.SIGNAL( \
+			"focusLost"), self.apply_edit_changes)
 		hbox = QtGui.QHBoxLayout()
 		hbox.addStretch()
 		hbox.setContentsMargins(0, 0, 0, 0)
 		hbox_widget = QtGui.QWidget()
 		hbox_widget.setLayout(hbox)
-
 		vbox = QtGui.QVBoxLayout()
 		vbox.addWidget(self.textedit_prepare)
 		vbox.addWidget(hbox_widget)
-
 		widget = QtGui.QWidget()
 		widget.setLayout(vbox)
+		tabwidget_script.addTab(widget, self.experiment.icon("inline_script"), \
+			"Prepare phase")
 
-		tabwidget_script.addTab(widget, self.experiment.icon("inline_script"), "Prepare phase")
-
-		self.textedit_run = inline_editor.inline_editor(self.experiment, notification=py_ver, syntax="python")
+		# Construct run editor
+		self.textedit_run = inline_editor.inline_editor(self.experiment, \
+			notification=py_ver, syntax="python")
 		self.textedit_run.apply.clicked.connect(self.apply_edit_changes)
-		QtCore.QObject.connect(self.textedit_run.edit, QtCore.SIGNAL("focusLost"), self.apply_edit_changes)
-
+		QtCore.QObject.connect(self.textedit_run.edit, QtCore.SIGNAL( \
+			"focusLost"), self.apply_edit_changes)
 		hbox = QtGui.QHBoxLayout()
 		hbox.addStretch()
 		hbox.setContentsMargins(0, 0, 0, 0)
 		hbox_widget = QtGui.QWidget()
 		hbox_widget.setLayout(hbox)
-
 		vbox = QtGui.QVBoxLayout()
 		vbox.addWidget(self.textedit_run)
 		vbox.addWidget(hbox_widget)
-
 		widget = QtGui.QWidget()
 		widget.setLayout(vbox)
-
-		tabwidget_script.addTab(widget, self.experiment.icon("inline_script"), "Run phase")
+		tabwidget_script.addTab(widget, self.experiment.icon("inline_script"), \
+			"Run phase")
 
 		# Switch to the run script by default, unless there is only content for
 		# the prepare script.
@@ -126,7 +128,8 @@ class inline_script(libopensesame.inline_script.inline_script, qtitem.qtitem):
 			tabwidget_script.setCurrentIndex(0)
 		else:
 			tabwidget_script.setCurrentIndex(1)
-
+			
+		# Add the tabwidget to the controls
 		self.edit_vbox.addWidget(tabwidget_script)
 
 	def edit_widget(self):
@@ -140,8 +143,10 @@ class inline_script(libopensesame.inline_script.inline_script, qtitem.qtitem):
 
 		qtitem.qtitem.edit_widget(self, False)
 		if not self.lock:
-			self.textedit_prepare.edit.setPlainText(self.unsanitize(str(self._prepare)))
-			self.textedit_run.edit.setPlainText(self.unsanitize(str(self._run)))
+			self.textedit_prepare.edit.setPlainText(self.unsanitize(str( \
+				self._prepare)))
+			self.textedit_run.edit.setPlainText(self.unsanitize(str( \
+				self._run)))
 		return self._edit_widget
 
 	def get_ready(self):
