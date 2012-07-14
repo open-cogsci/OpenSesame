500dfeec7348379df487be69adee1482d1b79765
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-07-14 14:51:06 +0200

    Improve general script editor

--- libqtopensesame/widgets/general_script_editor.py
+++ libqtopensesame/widgets/general_script_editor.py
@@ -17,12 +17,54 @@ You should have received a copy of the GNU General Public License
 along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+from PyQt4 import QtCore, QtGui
+from libqtopensesame.misc import _
 from libqtopensesame.widgets.inline_editor import inline_editor
+from libqtopensesame.ui.general_script_editor_ui import \
+	Ui_widget_general_script_editor
 
-class general_script_editor(inline_editor):
+class general_script_editor(QtGui.QWidget):
 
-	def __init__(self, parent=None):
+	"""The general script editor"""
+
+	def __init__(self, main_window):
 	
-		self.main_window = parent
-		inline_editor.__init__(self, self.main_window.experiment)		
+		"""
+		Constructor
+		
+		Arguments:
+		main_window -- the main window
+		"""	
+	
+		self.main_window = main_window
+		QtGui.QWidget.__init__(self, main_window)
+		self.ui = Ui_widget_general_script_editor()
+		self.ui.setupUi(self)
+		self.ui.edit = inline_editor(self.main_window.experiment)
+		self.ui.edit.applied.connect(self._apply)
+		self.ui.layout_vbox.addWidget(self.ui.edit)
+		self.main_window.theme.apply_theme(self)
 		self.tab_name = 'general_script_editor'
+		
+	def _apply(self):
+	
+		"""Confirm and apply the script changes"""
+		
+		resp = QtGui.QMessageBox.question(self.main_window, _('Apply?'), \
+			_('Are you sure you want to apply the changes to the general script?'), \
+			QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+		if resp == QtGui.QMessageBox.No:
+			return	
+		self.main_window.dispatch.event_regenerate.emit(self.ui.edit.getText())
+				
+	def on_activate(self):
+		
+		"""Refresh the tab when it is activated"""
+	
+		self.refresh()
+		
+	def refresh(self):
+	
+		"""Refresh the contents of the general script"""
+		
+		self.ui.edit.setText(self.main_window.experiment.to_string())
