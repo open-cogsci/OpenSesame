98faa963e731c1758bfe9b64ae737ad9f7d8db7a
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-09-23 19:26:57 +0200

    Change xpyriment mouse backend so that custom cursor is not allowed in fullscreen mode

--- openexp/_mouse/xpyriment.py
+++ openexp/_mouse/xpyriment.py
@@ -18,6 +18,7 @@ along with openexp.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import openexp._mouse.legacy
+from openexp import exceptions
 import pygame
 from pygame.locals import *
 from expyriment import stimuli
@@ -39,7 +40,10 @@ class xpyriment(openexp._mouse.legacy.legacy):
 		self.set_buttonlist(buttonlist)
 		self.set_timeout(timeout)
 		self.set_visible(visible)		
-		if self.experiment.get_check('custom_cursor', 'yes') == 'yes':
+		if self.experiment.get_check('custom_cursor', 'yes') == 'yes':			
+			if self.experiment.expyriment.screen._fullscreen:
+				raise exceptions.response_error( \
+					'The xpyriment mouse back-end does not support custom cursors in fullscreen mode (you can change this in the back-end settings)')
 			self.cursor = stimuli.Picture(self.experiment.resource( \
 				'cursor.png'))
 		else:
@@ -73,20 +77,28 @@ class xpyriment(openexp._mouse.legacy.legacy):
 			
 			# Draw a cusom cursor if necessary
 			if self.cursor != None and visible:
-				self.experiment.window.blit(bg_surface, (0,0))		
 				x, y = pygame.mouse.get_pos()
+				self.experiment.window.blit(bg_surface, (0,0))										
 				self.cursor.position = c2p((x+dx, y+dy))
-				self.cursor.present(clear=False)
+				self.cursor.present(clear=False)					
 			
 			# Process the input
-			for event in pygame.event.get():								
+			for event in pygame.event.get([MOUSEBUTTONDOWN, KEYDOWN]):												
 				if event.type == KEYDOWN and event.key == pygame.K_ESCAPE:
 					raise openexp.exceptions.response_error( \
 						"The escape key was pressed.")										
 				if event.type == MOUSEBUTTONDOWN:
 					if buttonlist == None or event.button in buttonlist:
 						pygame.mouse.set_visible(self.visible)
-						return event.button, event.pos, time
+						
+						# Compensate for the fact that the screen is padded
+						x, y = event.pos
+						x -= (self.experiment.expyriment.screen.window_size[0] \
+							-self.experiment.width)/2
+						y -= (self.experiment.expyriment.screen.window_size[1] \
+							-self.experiment.height)/2
+												
+						return event.button, (x,y), time					
 											
 		if self.cursor == None:
 			pygame.mouse.set_visible(self.visible)					
