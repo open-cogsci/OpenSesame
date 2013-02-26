204d5dc27aa2bef507e19c64d9cd3125a9bd0a3b
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-02-26 18:17:36 +0100

    Bump to 0.27.2~pre2

--- libopensesame/misc.py
+++ libopensesame/misc.py
@@ -21,7 +21,7 @@ import os
 import os.path
 import sys
 
-version = "0.27.2~pre1"
+version = "0.27.2~pre2"
 codename = "Frisky Freud"
 
 use_global_resources = "--no-global-resources" not in sys.argv
