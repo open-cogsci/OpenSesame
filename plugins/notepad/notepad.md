c7ec6ca2f9ed17e5ab8e71bc57c5e82de9cc2d76
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-04-10 11:27:08 +0200

    Convert notepad plug-in to new-style API

---
+++ plugins/notepad/notepad.md
@@ -0,0 +1,3 @@
+# Notepad
+
+The notepad is a simple plug-in that allows you to annotate your experiment. It does not offer any real functionality.
--- plugins/notepad/notepad.py
+++ plugins/notepad/notepad.py
@@ -15,88 +15,21 @@ You should have received a copy of the GNU General Public License
 along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from libopensesame import item
-from libqtopensesame import qtplugin
-import openexp.canvas
-import os.path
-from PyQt4 import QtGui, QtCore
+from libopensesame.item import item
+from libqtopensesame.items.qtautoplugin import qtautoplugin
 
-class notepad(item.item):
+class notepad(item):
 
 	"""Notepad plug-in"""
 
-	def __init__(self, name, experiment, string=None):
-	
-		"""
-		Constructor
-
-		Arguments:
-		name -- the name of the item
-		experiment -- the experiment
-
-		Keyword arguments:
-		string -- an item definition string (default = None)
-		"""
-		
-		self.item_type = "notepad"
-		self.note = "Type your note here"
-		self.description = \
-			"A simple notepad to document your experiment. This plug-in does nothing."
-		item.item.__init__(self, name, experiment, string)					
+	description = \
+			u'A simple notepad to document your experiment. This plug-in does nothing.'
 					
-class qtnotepad(notepad, qtplugin.qtplugin):
+class qtnotepad(notepad, qtautoplugin):
 
 	"""Notepad plug-in GUI"""
 
-	def __init__(self, name, experiment, string=None):
-	
-		"""
-		Constructor
+	def __init__(self, name, experiment, script=None):
 
-		Arguments:
-		name -- the name of the item
-		experiment -- the experiment
-
-		Keyword arguments:
-		string -- an item definition string (default = None)
-		"""
-		
-		notepad.__init__(self, name, experiment, string)		
-		qtplugin.qtplugin.__init__(self, __file__)	
-		
-	def init_edit_widget(self):
-	
-		"""Initialize controls"""
-		
-		self.lock = True		
-		qtplugin.qtplugin.init_edit_widget(self, False)		
-		self.add_editor_control("note", "Note", tooltip = "Type your note here")				
-		self.lock = True		
-		
-	def apply_edit_changes(self):
-	
-		"""
-		Apply controls
-		
-		Returns:
-		True on success, False on failure		
-		"""
-		
-		if not qtplugin.qtplugin.apply_edit_changes(self, False) or self.lock:
-			return False
-		self.experiment.main_window.refresh(self.name)		
-		return True
-
-	def edit_widget(self):
-	
-		"""
-		Update the controls
-		
-		Returns:
-		The QWidget containing the controls
-		"""
-		
-		self.lock = True
-		qtplugin.qtplugin.edit_widget(self)				
-		self.lock = False
-		return self._edit_widget
+		notepad.__init__(self, name, experiment, script)
+		qtautoplugin.__init__(self, __file__)
