f90cce9392716f204ca4257256efa6d6d8354a99
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-09-04 17:11:09 +0200

    Add `orientation` keyword to rating_scale form widget.

--- libopensesame/widgets/rating_scale.py
+++ libopensesame/widgets/rating_scale.py
@@ -25,7 +25,7 @@ class rating_scale(widget):
 
 	"""A simple rating scale/ Likert widget"""
 
-	def __init__(self, form, nodes=5, click_accepts=False, var=None, default=None):
+	def __init__(self, form, nodes=5, click_accepts=False, orientation=u'horizontal', var=None, default=None):
 	
 		"""<DOC>
 		Constructor.
@@ -40,6 +40,9 @@ class rating_scale(widget):
 							(default=5).
 		click_accepts	--	Indicates whether the form should close when a value #
 							is selected (default=False).
+		orientation		--	'horizontal' indicates a horizontally oriented
+							rating scale, 'vertical' indicates a vertically
+							oriented rating scale. (default=u'horizontal')
 		var				--	The name of the experimental variable that should be used to log #
 							the widget status. The value that is logged is the number of #
 							the node that was selected, with the first node being 0. If no #
@@ -48,10 +51,10 @@ class rating_scale(widget):
 							documentation page.(default=None).
 		default			--	The node that is selected by default, or None to #
 							select no node. The value corresponds to the node #
-							number, where 0 is the first node. (default=None)s
+							number, where 0 is the first node. (default=None)
 		</DOC>"""	
 		
-		if type(click_accepts) != bool:
+		if isinstance(click_accepts, basestring):
 			click_accepts = click_accepts == u'yes'
 		
 		widget.__init__(self, form)
@@ -60,6 +63,7 @@ class rating_scale(widget):
 		self.click_accepts = click_accepts
 		self.pos_list = []
 		self.var = var
+		self.orientation = orientation
 		if type(nodes) == int:
 			self.nodes = [u'']*nodes
 		elif isinstance(nodes, basestring):
@@ -95,24 +99,43 @@ class rating_scale(widget):
 		"""<DOC>
 		Draws the widget.
 		</DOC>"""	
-		
-		# Some ugly maths, but basically it evenly spaces the checkboxes and
-		# draws a frame around it.
-		x, y, w, h = self.rect		
-		cy = y+h/2
-		_h = self.form.theme_engine.box_size()				
-		dx = (1*w-3*_h)/(len(self.nodes)-1)										
-		self.form.theme_engine.frame(x, cy-.5*_h, w, 2*_h, style=u'light')		
-		_x = x+_h
-		i = 0
-		for node in self.nodes:
-			self.form.theme_engine.box(_x, cy, checked=(self.value == i))
-			text_height = self.form.canvas.text_size(node)[1]
-			self.form.canvas.text(node, center=True, x=_x+self.box_size/2, \
-				y=cy-text_height)						
-			self.pos_list.append( (_x, cy) )
-			_x += dx
-			i += 1
+
+		x, y, w, h = self.rect
+		cx = x+w/2
+		cy = y+h/2		
+		_h = self.form.theme_engine.box_size()		
+		if self.orientation == u'horizontal':
+			# Some ugly maths, but basically it evenly spaces the checkboxes and
+			# draws a frame around it.
+			dx = (1*w-3*_h)/(len(self.nodes)-1)
+			self.form.theme_engine.frame(x, cy-.5*_h, w, 2*_h, style=u'light')
+			_x = x+_h
+			i = 0
+			for node in self.nodes:
+				self.form.theme_engine.box(_x, cy, checked=(self.value == i))
+				text_height = self.form.canvas.text_size(node)[1]
+				self.form.canvas.text(node, center=True, x=_x+self.box_size/2, \
+					y=cy-text_height)
+				self.pos_list.append( (_x, cy) )
+				_x += dx
+				i += 1
+		elif self.orientation == u'vertical':
+			dy = (1*h-3*_h)/(len(self.nodes)-1)
+			self.form.theme_engine.frame(cx-.5*_h, y, 2*_h, h, style=u'light')
+			_y = y+_h
+			i = 0
+			for node in self.nodes:
+				self.form.theme_engine.box(cx, _y, checked=(self.value == i))
+				text_width = self.form.canvas.text_size(node)[0]
+				self.form.canvas.text(node, center=True, x=cx-text_width,
+					y=_y+self.box_size/2)
+				self.pos_list.append( (cx, _y) )
+				_y += dy
+				i += 1
+		else:
+			raise form_error( \
+				u'rating_scale orientation must be "horizontal" or "vertical", not "%s"' % \
+				self.orientation)
 			
 	def set_value(self, val):
 	
@@ -124,7 +147,8 @@ class rating_scale(widget):
 		</DOC>"""
 		
 		if val != None and (val >= len(self.nodes) or val < 0):
-			raise form_error(u'Trying to select a non-existing node (%s). Did you specify an incorrect default value?' \
+			raise form_error( \
+				u'Trying to select a non-existing node (%s). Did you specify an incorrect default value?' \
 				% val)
 		self.value = val
 		self.set_var(val)
