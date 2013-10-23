99da59ad86ce4434a6ae6781f46971a863be7a4f
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-10-23 18:15:26 +0200

    Bump to 2.8.0~pre5

--- libopensesame/misc.py
+++ libopensesame/misc.py
@@ -21,7 +21,7 @@ import os
 import os.path
 import sys
 
-version = u'2.8.0~pre4'
+version = u'2.8.0~pre5'
 codename = u'Gutsy Gibson'
 
 use_global_resources = '--no-global-resources' not in sys.argv
