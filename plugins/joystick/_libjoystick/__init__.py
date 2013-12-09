65dcc2cbc6c041465fc3f9b8a4167c15debf6a09
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-12-09 16:27:35 +0100

    First step to making joystick plug-in backend independent - Necessary due to system freezes when using in combination with PsychoPy back-end

--- plugins/joystick/_libjoystick/__init__.py
+++ plugins/joystick/_libjoystick/__init__.py
@@ -16,24 +16,3 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with openexp.  If not, see <http://www.gnu.org/licenses/>.
 """
-
-import os
-import imp
-from libopensesame import debug
-
-class libjoystick:
-
-	"""Morphs into the appropriate class form the _libjoystick module."""
-
-	def __init__(self, experiment, joybuttonlist=None, timeout=None):
-	
-		backend = u'legacy'
-		debug.msg(u'morphing into %s' % backend)
-		mod_path = os.path.join(os.path.dirname(__file__), u'_libjoystick', \
-			u'%s.py' % backend)
-		debug.msg(u'loading %s' % mod_path)
-		mod = imp.load_source(backend, mod_path)
-		cls = getattr(mod, backend)
-		self.__class__ = cls
-		cls.__init__(self, experiment, joybuttonlist=joybuttonlist, timeout= \
-			timeout)
