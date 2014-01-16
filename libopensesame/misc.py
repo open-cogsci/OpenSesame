7bab1a0456637b9241caecb0430d02e163f1e13b
Sebastiaan Mathot <s.mathot@cogsci.nl>
2014-01-16 15:33:58 +0100

    Set subject_parity in Android runtime (closes #221) - Clean up Android runtime a bit - Bump to 2.8.0~pre16

--- libopensesame/misc.py
+++ libopensesame/misc.py
@@ -21,7 +21,7 @@ import os
 import os.path
 import sys
 
-version = u'2.8.0~pre15'
+version = u'2.8.0~pre16'
 codename = u'Gutsy Gibson'
 
 use_global_resources = '--no-global-resources' not in sys.argv
