53c69c09c0ec203159e9309f56eb1fbff4b40a1f
Sebastiaan Mathot <s.mathot@cogsci.nl>
2014-08-14 18:39:44 +0200

    Further icon-name fixes

--- libqtopensesame/items/keyboard_response.py
+++ libqtopensesame/items/keyboard_response.py
@@ -63,7 +63,7 @@ class keyboard_response(keyboard_response_runtime, qtplugin):
 		self.add_checkbox_control('flush', 'Flush pending keypresses',
 			tooltip='Flush pending keypresses')
 		# List available keys
-		button_list_keys = QtGui.QPushButton(self.experiment.icon("info"),
+		button_list_keys = QtGui.QPushButton(self.experiment.icon("help-about"),
 			"List available keys")
 		button_list_keys.setIconSize(QtCore.QSize(16,16))
 		button_list_keys.clicked.connect(self.list_keys)
