496ed949389187f0ed3654cc4e5b9d5845bb86a6
Sebastiaan Mathot <s.mathot@cogsci.nl>
2014-09-05 11:45:13 +0200

    Bump to 2.9.0~pre9

--- libopensesame/misc.py
+++ libopensesame/misc.py
@@ -21,7 +21,7 @@ import os
 import os.path
 import sys
 
-version = u'2.9.0~pre8'
+version = u'2.9.0~pre9'
 codename = u'Hesitant Heisenberg'
 
 use_global_resources = '--no-global-resources' not in sys.argv
