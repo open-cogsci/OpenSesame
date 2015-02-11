546683e89e42077b561158f0c529018d27bde37d
Sebastiaan Mathot <s.mathot@cogsci.nl>
2015-02-11 11:35:56 +0100

    Bump to 2.9.4~pre1 - Prevent indirect recursion errors in overview area

--- libopensesame/misc.py
+++ libopensesame/misc.py
@@ -21,7 +21,7 @@ import os
 import os.path
 import sys
 
-version = u'2.9.3'
+version = u'2.9.4~pre1'
 codename = u'Hesitant Heisenberg'
 
 use_global_resources = '--no-global-resources' not in sys.argv
