bee9ac2f35155894ba15222d8f9f053e274e5972
Sebastiaan Mathot <s.mathot@cogsci.nl>
2015-03-12 13:00:34 +0100

    Improve file-pool behavior - Clean up old pool when opening file - Show warning when pool is too large

--- libqtopensesame/qtopensesame.py
+++ libqtopensesame/qtopensesame.py
@@ -675,6 +675,8 @@ class qtopensesame(QtGui.QMainWindow, base_component):
 			self.print_debug_window(e)
 			self.experiment.notify(e.html(), title=u'Exception')
 			return
+		libopensesame.experiment.clean_up(verbose=debug.enabled,
+			keep=[exp.pool_folder])
 		self.experiment = exp
 		self.experiment.build_item_tree()
 		self.ui.tabwidget.open_general()
