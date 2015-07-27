8f39a84253e666b7a606b4d5fe5edb39ce3357ca
Sebastiaan Mathot <s.mathot@cogsci.nl>
2015-07-27 14:42:45 +0200

    Android runtime: Catch exceptions when opening experiment - Makes these exceptions visible in the debug output - Closes #346

--- opensesameandroid.py
+++ opensesameandroid.py
@@ -105,7 +105,11 @@ def main():
 		logfile = menu._logfile
 
 	# Next run the actual experiment!
-	exp = experiment('Experiment', experiment_path)
+	try:
+		exp = experiment('Experiment', experiment_path)
+	except Exception as e:
+		for s in traceback.format_exc(e).split("\n"):
+			print(s)
 	print('Launching %s' % experiment_path)
 	exp.set_subject(subject_nr)
 	exp.logfile = logfile
