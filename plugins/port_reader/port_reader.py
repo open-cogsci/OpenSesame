bec94010f2faaf914b97108c5c60f591ccbbe83d
Sebastiaan Mathot <s.mathot@cogsci.nl>
2015-08-04 14:58:07 +0200

    Use var_store in port reader - Avoids warnings, but desperately needs testing

--- plugins/port_reader/port_reader.py
+++ plugins/port_reader/port_reader.py
@@ -29,36 +29,20 @@ class port_reader(item.item, generic_response.generic_response):
 
 	"""A plug-in to collect responses from a port (Window only)"""
 
-	def __init__(self, name, experiment, string=None):
-	
-		"""
-		Constructor
-		
-		Arguments:
-		name -- item name
-		experiment -- experiment instance
-		
-		Keyword arguments:
-		string -- definition string (default=None)
-		"""
-		
-		# The item_typeshould match the name of the module
-		self.item_type = "port_reader"
-		
-		# Provide a short accurate description of the items functionality
-		self.description = \
-			"Collects input from a port for the purpose of response collection"
-		
-		self.timeout = "infinite"
-		self.port = 889
-		self.resting_value = 127
-		self.auto_response = 255 # 'a'
-		self.duration = "portinput"
-		self.dummy = "no"
-		self.process_feedback = True				
-		
-		# The parent handles the rest of the contruction
-		item.item.__init__(self, name, experiment, string)
+	description = \
+		u"Collects input from a port for the purpose of response collection"
+
+	def reset(self):
+
+		"""item.item."""
+
+		self.var.timeout = "infinite"
+		self.var.port = 889
+		self.var.resting_value = 127
+		self.var.auto_response = 255 # 'a'
+		self.var.duration = "portinput"
+		self.var.dummy = "no"
+		self.process_feedback = True
 
 	def get_portinput(self):
 
@@ -94,9 +78,9 @@ class port_reader(item.item, generic_response.generic_response):
 
 		"""Process a portinput response"""
 
-		self.experiment._start_response_interval = self.sri			
-		self.experiment.response, self.experiment.end_response_interval = retval		
-								
+		self.experiment._start_response_interval = self.sri
+		self.experiment.response, self.experiment.end_response_interval = retval
+
 	def prepare(self):
 
 		"""
@@ -123,7 +107,7 @@ class port_reader(item.item, generic_response.generic_response):
 		"""
 
 		# Record the onset of the current item
-		self.set_item_onset()	
+		self.set_item_onset()
 		self.set_sri()
 		self.process_response()
 
@@ -131,51 +115,51 @@ class port_reader(item.item, generic_response.generic_response):
 		return True
 
 	def var_info(self):
-		
+
 		"""
 		Give a list of dictionaries with variable descriptions
 
 		Returns:
 		A list of (name, description) tuples
-		"""		
+		"""
 
 		return item.item.var_info(self) + \
 			generic_response.generic_response.var_info(self)
-					
+
 class qtport_reader(port_reader, qtplugin.qtplugin):
 
 	"""GUI controls for port_reader plug-in"""
 
 	def __init__(self, name, experiment, string=None):
-	
+
 		"""
 		Constructor
-		
+
 		Arguments:
 		name -- item name
 		experiment -- experiment instance
-		
+
 		Keyword arguments:
 		string -- definition string (default=None)
 		"""
-		
-		# Pass the word on to the parents		
-		port_reader.__init__(self, name, experiment, string)		
-		qtplugin.qtplugin.__init__(self, __file__)	
-		
+
+		# Pass the word on to the parents
+		port_reader.__init__(self, name, experiment, string)
+		qtplugin.qtplugin.__init__(self, __file__)
+
 	def init_edit_widget(self):
-	
+
 		"""Initialize GUI controls"""
-		
+
 		# Lock the widget until we're doing creating it
 		self.lock = True
-		
-		# Pass the word on to the parent		
+
+		# Pass the word on to the parent
 		qtplugin.qtplugin.init_edit_widget(self, False)
-		
+
 		# Create the controls
-		# 
-		# A number of convenience functions are available which 
+		#
+		# A number of convenience functions are available which
 		# automatically create controls, which are also automatically
 		# updated and applied. If you set the varname to None, the
 		# controls will be created, but not automatically updated
@@ -184,9 +168,9 @@ class qtport_reader(port_reader, qtplugin.qtplugin):
 		# qtplugin.add_combobox_control(varname, label, list_of_options)
 		# - creates a QComboBox
 		# qtplugin.add_line_edit_control(varname, label)
-		# - creates a QLineEdit		
+		# - creates a QLineEdit
 		# qtplugin.add_spinbox_control(varname, label, min, max, suffix = suffix, prefix = prefix)
-		
+
 		self.add_combobox_control("dummy", \
 			"Dummy mode (use keyboard instead)", ["yes", "no"], \
 			tooltip="Enable dummy mode for testing purposes")
@@ -204,38 +188,38 @@ class qtport_reader(port_reader, qtplugin.qtplugin):
 		# Add a stretch to the edit_vbox, so that the controls do not
 		# stretch to the bottom of the window.
 		self.edit_vbox.addStretch()
-		
+
 		# Unlock
-		self.lock = True		
-		
+		self.lock = True
+
 	def apply_edit_changes(self):
-	
+
 		"""Apply GUI controls"""
-		
+
 		# Abort if the parent reports failure of if the controls are locked
 		if not qtplugin.qtplugin.apply_edit_changes(self, False) or self.lock:
 			return False
-				
+
 		# Refresh the main window, so that changes become visible everywhere
-		self.experiment.main_window.refresh(self.name)		
-		
+		self.experiment.main_window.refresh(self.name)
+
 		# Report success
 		return True
 
 	def edit_widget(self):
-	
+
 		"""Update GUI controls"""
-		
+
 		# Lock the controls, otherwise a recursive loop might aris
 		# in which updating the controls causes the variables to be
 		# updated, which causes the controls to be updated, etc...
 		self.lock = True
-		
+
 		# Let the parent handle everything
-		qtplugin.qtplugin.edit_widget(self)				
-		
+		qtplugin.qtplugin.edit_widget(self)
+
 		# Unlock
 		self.lock = False
-		
+
 		# Return the _edit_widget
 		return self._edit_widget
