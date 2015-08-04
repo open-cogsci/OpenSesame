b536ce7b0cce91e444e03c6e27bdf8b1e637f192
Sebastiaan Mathot <s.mathot@cogsci.nl>
2015-08-04 18:29:31 +0200

    Update external_script item

---
+++ plugins/external_script/external_script.md
@@ -0,0 +1,39 @@
+# External_script
+
+The `external_script` item is an alternative way to include Python code in your experiment. Whereas the `inline_script` allows you to enter Python code directly, the `external_script` item runs a Python script from a file.
+
+## Options
+
+- *Script file* is the name of the Python script that is executed.
+- *Prepare function in script* is the name of the function in the script that is called (without parameters) during the prepare phase.
+- *Run function in script* is the name of the function in the script that is called (without parameters) during the run phase.
+
+## Script execution and Python workspace
+
+The script is executed in the same Python workspace that is used for `inline_script` items. This means that you can access all the regular objects  and functions, just like you could from an `inline_script`.
+
+When the plug-in is first initialized (during the first prepare phase), the entire script is executed.
+
+## Example
+
+Consider the following script, assuming that you use `prepare` and `run` as the functions to run:
+
+~~~ .python
+print('This will be shown when the plug-in is initialized')
+
+def prepare():
+
+    print('This will be shown during the prepare phase')
+    global c
+    c = canvas()
+    c.fixdot()
+
+def run():
+
+    print('This will be shown during the run phase')
+    global c
+    c.show()
+    clock.sleep(1000)
+    c.clear()
+    c.show()
+~~~
--- plugins/external_script/external_script.py
+++ plugins/external_script/external_script.py
@@ -16,191 +16,58 @@ along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from libopensesame.exceptions import osexception
-from libopensesame import item
-from libqtopensesame import qtplugin, pool_widget
-import openexp.canvas
-import os.path
-from PyQt4 import QtGui, QtCore
+from libopensesame.item import item
+from libqtopensesame.items.qtautoplugin import qtautoplugin
 import imp
 
-class external_script(item.item):
+class external_script(item):
 
-	"""
-	This class (the class with the same name as the module)
-	handles the basic functionality of the item. It does
-	not deal with GUI stuff.
-	"""
+	description = \
+		u"Run Python code directly from a script file"
 
-	def __init__(self, name, experiment, string = None):
-	
+	def reset(self):
+
 		"""
-		Constructor
+		desc:
+			Resets plug-in to initial state.
 		"""
-		
-		# The item_typeshould match the name of the module
-		self.item_type = "external_script"
-		self.file = ""
+
 		self.module = None
-		self.prepare_func = "prepare"
-		self.run_func = "run"
-		
-		# Provide a short accurate description of the items functionality
-		self.description = "An alternative way of running Python code, directly from a script file"
-				
-		# The parent handles the rest of the contruction
-		item.item.__init__(self, name, experiment, string)
-						
+		self.var.file = u''
+		self.var.prepare_func = u'prepare'
+		self.var.run_func = u'run'
+
 	def prepare(self):
-	
+
 		"""
-		Prepare the item. In this case this means drawing a fixation
-		dot to an offline canvas.
+		desc:
+			Prepares the item.
 		"""
-		
-		# Pass the word on to the parent
-		item.item.prepare(self)
-		
+
+		item.prepare(self)
 		if self.module is None:
-			try:
-				self.module = imp.load_source("file", os.path.join( \
-					self.experiment.pool.folder(), self.file))
-			except Exception as e:
-				raise osexception( \
-					"Failed to import '%s' in the prepare phase of external_script item '%s': %s" \
-					% (self.file, self.name, e))
-			
-		try:
-			getattr(self.module, self.prepare_func)(self)
-		except Exception as e:
-			raise osexception( \
-				"Failed to run function '%s(item)' in the prepare phase of external_script item '%s': %s" \
-				% (self.prepare_func, self.name, e))				
-		
-		# Report success
-		return True
-				
+			path = self.experiment.pool[self.var.file]
+			self.experiment.python_workspace.run_file(path)
+		self.prepare_bytecode = self.experiment.python_workspace._compile(
+			u'%s()' % self.var.prepare_func)
+		self.run_bytecode = self.experiment.python_workspace._compile(
+			u'%s()' % self.var.run_func)
+		self.experiment.python_workspace._exec(self.prepare_bytecode)
+
 	def run(self):
-	
+
 		"""
-		Run the item. In this case this means putting the offline canvas
-		to the display and waiting for the specified duration.
+		desc:
+			Runs the item.
 		"""
-		
-		# Show the canvas
+
 		self.set_item_onset()
-			
-		try:
-			getattr(self.module, self.run_func)(self)
-		except Exception as e:
-			raise osexception( \
-				"Failed to run function '%s(item)' in the run phase of external_script item '%s': %s" \
-				% (self.run_func, self.name, e))
-								
-		# Report success
-		return True
-					
-class qtexternal_script(external_script, qtplugin.qtplugin):
+		self.experiment.python_workspace._exec(self.run_bytecode)
 
-	"""
-	This class (the class named qt[name of module] handles
-	the GUI part of the plugin. For more information about
-	GUI programming using PyQt4, see:
-	<http://www.riverbankcomputing.co.uk/static/Docs/PyQt4/html/classes.html>
-	"""
+class qtexternal_script(external_script, qtautoplugin):
 
-	def __init__(self, name, experiment, string = None):
-	
-		"""
-		Constructor
-		"""
-		
-		# Pass the word on to the parents		
-		external_script.__init__(self, name, experiment, string)		
-		qtplugin.qtplugin.__init__(self, __file__)	
-		
-	def init_edit_widget(self):
-	
-		"""
-		This function creates the controls for the edit
-		widget.
-		"""
-		
-		# Lock the widget until we're doing creating it
-		self.lock = True
-		
-		# Pass the word on to the parent		
-		qtplugin.qtplugin.init_edit_widget(self, False)
-		
-		# Create the controls
-		# 
-		# A number of convenience functions are available which 
-		# automatically create controls, which are also automatically
-		# updated and applied. If you set the varname to None, the
-		# controls will be created, but not automatically updated
-		# and applied.
-		#
-		# qtplugin.add_combobox_control(varname, label, list_of_options)
-		# - creates a QComboBox
-		# qtplugin.add_line_edit_control(varname, label)
-		# - creates a QLineEdit		
-		# qtplugin.add_spinbox_control(varname, label, min, max, suffix = suffix, prefix = prefix)
-		
-		self.add_filepool_control("file", "Script file", self.browse_script, default = "", tooltip = "A Python (.py) script")	
-		self.add_line_edit_control("prepare_func", "Prepare function in script", default = "prepare", tooltip = "A function with a single parameter, e.g., 'def run(item)'")		
-		self.add_line_edit_control("run_func", "Run function in script", default = "run", tooltip = "A function with a single parameter, e.g., 'def run(item)'")		
-		
-		# Add a stretch to the edit_vbox, so that the controls do not
-		# stretch to the bottom of the window.
-		self.edit_vbox.addStretch()		
-		
-		# Unlock
-		self.lock = True		
-		
-	def browse_script(self):
-	
-		"""
-		Browse the filepool
-		"""
-		
-		s = pool_widget.select_from_pool(self.experiment.main_window)
-		if s == "":
-			return
-			
-		self.auto_line_edit["file"].setText(s)			
-		self.apply_edit_changes()							
-		
-	def apply_edit_changes(self):
-	
-		"""
-		Set the variables based on the controls
-		"""
-		
-		# Abort if the parent reports failure of if the controls are locked
-		if not qtplugin.qtplugin.apply_edit_changes(self, False) or self.lock:
-			return False
-				
-		# Refresh the main window, so that changes become visible everywhere
-		self.experiment.main_window.refresh(self.name)		
-		
-		# Report success
-		return True
+	def __init__(self, name, experiment, script=None):
 
-	def edit_widget(self):
-	
-		"""
-		Set the controls based on the variables
-		"""
-		
-		# Lock the controls, otherwise a recursive loop might aris
-		# in which updating the controls causes the variables to be
-		# updated, which causes the controls to be updated, etc...
-		self.lock = True
-		
-		# Let the parent handle everything
-		qtplugin.qtplugin.edit_widget(self)				
-		
-		# Unlock
-		self.lock = False
-		
-		# Return the _edit_widget
-		return self._edit_widget
+		# Call parent constructors.
+		external_script.__init__(self, name, experiment, script)
+		qtautoplugin.__init__(self, __file__)
