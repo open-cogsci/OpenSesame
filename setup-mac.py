c783e2afc20f45200216ff46e47bc7a86042b346
Daniel Schreij <dschreij@gmail.com>
2016-02-25 15:55:14 +0100

    Syntax error in setup-mac.py file (sorry)

--- setup-mac.py
+++ setup-mac.py
@@ -115,7 +115,7 @@ for extension in included_extensions:
 for plugin in included_plugins:
 	shutil.copytree(os.path.join("plugins",plugin), os.path.join("dist/opensesame.app/Contents/Resources/plugins/",plugin))
 
-Anaconda libpng version is too old for pygame (min required version is 36). Copy homebrew version instead
+# Anaconda libpng version is too old for pygame (min required version is 36). Copy homebrew version instead
 try:
 	shutil.copy('/usr/local/opt/libpng/lib/libpng16.16.dylib','dist/OpenSesame.app/Contents/Frameworks/libpng16.16.dylib')
 except:
