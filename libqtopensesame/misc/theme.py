571dd57a1e3ab02da296818a56168bf02de17938
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-03-22 13:24:43 +0100

    Replace old theme - Now in separate repository

--- libqtopensesame/misc/theme.py
+++ libqtopensesame/misc/theme.py
@@ -25,7 +25,7 @@ from libopensesame import debug, misc
 from libqtopensesame.misc import config
 from qtpy import QtGui, QtWidgets, QtCore
 
-available_themes = [u'default', u'moka']
+available_themes = [u'default']
 
 class theme:
 
--- resources/theme/default/__theme__.py
+++ resources/theme/default/__theme__.py
@@ -17,9 +17,6 @@ You should have received a copy of the GNU General Public License
 along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__author__ = "Sebastiaan Mathot"
-__license__ = "GPLv3"
-
 icon_map = "icon_map.csv"
-icon_theme = "os-custom-icons"
+icon_theme = "MokaSesame"
 qss = "stylesheet.qss"
