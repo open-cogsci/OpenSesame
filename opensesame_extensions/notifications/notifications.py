ca53953a4eb9bac346eaa124fb4abca67dff9791
Daniel Schreij <dschreij@gmail.com>
2016-04-15 17:29:38 +0200

    Give notifications an expiration time

--- opensesame_extensions/notifications/notifications.py
+++ opensesame_extensions/notifications/notifications.py
@@ -22,6 +22,7 @@ along with this module. If not, see <http://www.apache.org/licenses/>.
 import QNotifications
 from libopensesame.py3compat import *
 from libqtopensesame.extensions import base_extension
+import time
 
 __author__ = u"Daniel Schreij"
 __license__ = u"GPLv3"
@@ -29,19 +30,27 @@ __license__ = u"GPLv3"
 class notifications(base_extension):
 
 	def event_startup(self):
+		self.old_notifications = {}
+		self.expiration_time = 60 # a minute
 
-		self.old_notifications = []
 		self.notification_area = QNotifications.QNotificationArea(
 			self.tabwidget, useGlobalCSS=True)
 		self.notification_area.setEntryEffect(u'fadeIn', 200)
 		self.notification_area.setExitEffect(u'fadeOut', 200)
 
-	def event_notify(self, message, category='primary', timeout=5000):
+	def event_notify(self, message, category='primary', timeout=5000, always_show=False):
 		""" Show a notification 'message' in the style 'notification type' for
 		'timeout' milliseconds (where 0 milliseconds displays the notification
 		indefinitely, until the user removes it)."""
+		current_time = time.time()
 
-		if (message, category, timeout) in self.old_notifications:
-			return
-		self.old_notifications.append( (message, category, timeout) )
+		# See if notification has been shown before. If it is within 
+		# self.expiration_time, don't show it again.
+		if not always_show:
+			if (message, category, timeout) in self.old_notifications:
+				prev_time = self.old_notifications[(message, category, timeout)]
+				if current_time - prev_time < self.expiration_time:
+					return
+			# Add notification to old notifications list.
+			self.old_notifications[(message, category, timeout)] = current_time
 		self.notification_area.display(message, category, timeout)
