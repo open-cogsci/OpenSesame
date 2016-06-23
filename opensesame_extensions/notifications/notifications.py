74727986a63d6969719fcd319fb9c6d6ac8b6b39
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-06-23 15:57:12 +0200

    notifications: add buttontext

--- opensesame_extensions/notifications/notifications.py
+++ opensesame_extensions/notifications/notifications.py
@@ -39,13 +39,14 @@ class notifications(base_extension):
 		self.notification_area.setEntryEffect(u'fadeIn', 200)
 		self.notification_area.setExitEffect(u'fadeOut', 200)
 
-	def event_notify(self, message, category='primary', timeout=5000, always_show=False):
+	def event_notify(self, message, category='primary', timeout=5000,
+		always_show=False, buttontext=None):
 		""" Show a notification 'message' in the style 'notification type' for
 		'timeout' milliseconds (where 0 milliseconds displays the notification
 		indefinitely, until the user removes it)."""
 		current_time = time.time()
 
-		# See if notification has been shown before. If it is within 
+		# See if notification has been shown before. If it is within
 		# self.expiration_time, don't show it again.
 		if not always_show:
 			if (message, category, timeout) in self.old_notifications:
@@ -53,5 +54,5 @@ class notifications(base_extension):
 				if current_time - prev_time < self.expiration_time:
 					return
 			# Add notification to old notifications list.
-			self.old_notifications[(message, category, timeout)] = current_time
-		self.notification_area.display(message, category, timeout)
+			self.old_notifications[(message, category, timeout, buttontext)] = current_time
+		self.notification_area.display(message, category, timeout, buttontext)
