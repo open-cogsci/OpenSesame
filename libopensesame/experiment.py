open-cogsci/OpenSesame

commit bd6b6d4839eb5cf92c81b91d67ba77e6df12b845

variables: clear variable store when resetting items - fixes a bug where variables couldn't deleted by removing them from the item script

libopensesame/experiment.py
@@ -121,44 +121,8 @@ class experiment(item.item):
 		self.resources = resources
 		self.paused = False
 		self.output_channel = None
-
-		# Set default variables
-		self.var.start = u'experiment'
-		self.var.title = self.default_title
-		self.var.bidi = u'no'
-		self.var.round_decimals = 2
-		self.var.form_clicks = u'no'
-		self.var.disable_garbage_collection = u'yes'
-		# In version 2.9.X and before, the sketchpad used 0,0 for the screen
-		# center, whereas scripting items used 0,0 for the top-left. By setting
-		# uniform_coordinates to 'yes', 0,0 is used for the center in all cases.
-		self.var.uniform_coordinates = u'no'
-
-		# Sound parameters
-		self.var.sound_freq = 48000
-		self.var.sound_sample_size = -16 # Negative values mean signed
-		self.var.sound_channels = 2
-		self.var.sound_buf_size = 1024
-
-		# Default backend
-		self.var.canvas_backend = u'xpyriment'
-
-		# Display parameters
-		self.var.width = 1024
-		self.var.height = 768
-		self.var.background = u'black'
-		self.var.foreground = u'white'
-		if fullscreen:
-			self.var.fullscreen = u'yes'
-		else:
-			self.var.fullscreen = u'no'
-
-		# Font parameters
-		self.var.font_size = 18
-		self.var.font_family = u'mono'
-		self.var.font_italic = u'no'
-		self.var.font_bold = u'no'
-		self.var.font_underline = u'no'
+		self._fullscreen = fullscreen
+		self.reset()
 
 		# Logfile parameters
 		self._log = None
@@ -218,6 +182,45 @@ class experiment(item.item):
 			u'experiment.get_file() is deprecated. Use file_pool_store instead.',
 			DeprecationWarning)
 		return self.pool[path]
+
+	def reset(self):
+
+		"""See item."""
+
+		# Set default variables
+		self.var.start = u'experiment'
+		self.var.title = self.default_title
+		self.var.description = u'The main experiment item'
+		self.var.bidi = u'no'
+		self.var.round_decimals = 2
+		self.var.form_clicks = u'no'
+		self.var.disable_garbage_collection = u'yes'
+		# In version 2.9.X and before, the sketchpad used 0,0 for the screen
+		# center, whereas scripting items used 0,0 for the top-left. By setting
+		# uniform_coordinates to 'yes', 0,0 is used for the center in all cases.
+		self.var.uniform_coordinates = u'no'
+		# Sound parameters
+		self.var.sound_freq = 48000
+		self.var.sound_sample_size = -16 # Negative values mean signed
+		self.var.sound_channels = 2
+		self.var.sound_buf_size = 1024
+		# Default backend
+		self.var.canvas_backend = u'xpyriment'
+		# Display parameters
+		self.var.width = 1024
+		self.var.height = 768
+		self.var.background = u'black'
+		self.var.foreground = u'white'
+		if self._fullscreen:
+			self.var.fullscreen = u'yes'
+		else:
+			self.var.fullscreen = u'no'
+		# Font parameters
+		self.var.font_size = 18
+		self.var.font_family = u'mono'
+		self.var.font_italic = u'no'
+		self.var.font_bold = u'no'
+		self.var.font_underline = u'no'
 
 	def file_in_pool(self, path):
 
@@ -309,7 +312,8 @@ class experiment(item.item):
 			string:	The definition string.
 		"""
 
-		self.variables = {}
+		self.var.clear(preserve=[u'experiment_path', u'experiment_file'])
+		self.reset()
 		self.comments = []
 		debug.msg(u"building experiment")
 		if string is None:

libopensesame/item.py
@@ -282,7 +282,7 @@ class item(object):
 
 		debug.msg()
 		textblock_var = None
-		self.variables = {}
+		self.var.clear()
 		self.reset()
 		self.comments = []
 		if string is None:

libopensesame/logger.py
@@ -58,7 +58,7 @@ class logger(item.item):
 
 		"""See item."""
 
-		self.variables = {}
+		self.var.clear()
 		self.comments = []
 		self.reset()
 		if string is None:

libopensesame/loop.py
@@ -65,7 +65,7 @@ class loop(item.item):
 
 		"""See item."""
 
-		self.variables = {}
+		self.var.clear()
 		self.comments = []
 		self.reset()
 		if string is None:

libopensesame/sequence.py
@@ -88,7 +88,7 @@ class sequence(item.item):
 		string 	--	A definition string.
 		"""
 
-		self.variables = {}
+		self.var.clear()
 		self.comments = []
 		self.reset()
 		if string is None:

libopensesame/sketchpad.py
@@ -62,7 +62,7 @@ class sketchpad(base_response_item, keyboard_response_mixin,
 
 		"""See item."""
 
-		self.variables = {}
+		self.var.clear()
 		self.comments = []
 		self.reset()
 		if string is None:

libopensesame/var_store.py
@@ -148,6 +148,24 @@ class var_store(object):
 
 		self.__vars__[var] = val
 
+	def clear(self, preserve=[]):
+
+		"""
+		desc: |
+			*New in 3.1.2*
+
+			Clears all experimentals variables.
+
+		keywords:
+			preserve:
+				desc:	A list of variable names that shouldn't be cleared.
+				type:	list
+		"""
+
+		for var in list(self.__vars__.keys()):
+			if var not in preserve:
+				del self.__vars__[var]
+
 	def get(self, var, default=None, _eval=True, valid=None):
 
 		"""

libqtopensesame/misc/qtitem_store.py
@@ -106,7 +106,13 @@ class qtitem_store(item_store):
 		if warning_list:
 			import yaml
 			import os
-			self.tabwidget.open_help(os.path.join(u'help', u'new_item_warning'))
+			try:
+				self.tabwidget.open_help(
+					os.path.join(u'help', u'new_item_warning'))
+			except AttributeError:
+				# In case the experiment object doesn't exist yet, which causes
+				# the help page to fail.
+				pass
 			self.console.write(yaml.dump(warning_list))
 		self.main_window.set_unsaved(True)
 		return item

commit ddb8b6ab19a5c96a1b36b9612d1fd2fc6ed339ed

experiment: fix fullscreen - fixes a crucial regression introduced in bd6b6d4 in which fullscreen was overridden when parsing the experiment script

libopensesame/experiment.py
@@ -123,7 +123,6 @@ class experiment(item.item):
 		self.resources = resources
 		self.paused = False
 		self.output_channel = None
-		self._fullscreen = fullscreen
 		self.reset()
 
 		# Logfile parameters
@@ -141,6 +140,9 @@ class experiment(item.item):
 		item.item.__init__(self, name, self, string)
 		# Default subject info
 		self.set_subject(subject_nr)
+		# Fullscreen needs to be set after the experiment has been parsed from
+		# script, otherwise it will be overridden by the script.
+		self.var.fullscreen = u'yes' if fullscreen else u'no'
 		# Restore experiment path, which is either the full path (including
 		# filename), only the folder, or None.
 		if experiment_path is not None:
@@ -212,10 +214,6 @@ class experiment(item.item):
 		self.var.height = 768
 		self.var.background = u'black'
 		self.var.foreground = u'white'
-		if self._fullscreen:
-			self.var.fullscreen = u'yes'
-		else:
-			self.var.fullscreen = u'no'
 		# Font parameters
 		self.var.font_size = 18
 		self.var.font_family = u'mono'