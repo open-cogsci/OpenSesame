open-cogsci/OpenSesame

commit afb5a45040758a84f2856cc93febc6cdb00646c5

Implement safe_open() to open text files in utf-8 - Should fix #505 - Pending testing

libqtopensesame/misc/base_component.py
@@ -88,12 +88,8 @@ class base_component(base_qtobject):
 					from libopensesame import misc
 					ui_path = misc.resource(os.path.join(*path_list)+u'.ui')
 			debug.msg(u'dynamically loading ui: %s' % ui_path)
-			if py3:
-				with open(ui_path, encoding=u'utf-8') as fd:
-					self.ui = uic.loadUi(fd, self)
-			else:
-				with open(ui_path) as fd:
-					self.ui = uic.loadUi(fd, self)
+			with safe_open(ui_path) as fd:
+				self.ui = uic.loadUi(fd, self)
 		else:
 			self.ui = None
 

libopensesame/experiment.py
@@ -564,16 +564,16 @@ class experiment(item.item):
 		# If there are no files in the pool, save the script as plain text
 		if self.pool.count_included() == 0:
 			debug.msg(u'saving as plain text (without file pool)')
-			with open(path, u'w') as fd:
-				fd.write(safe_str(self.to_string()))
+			with safe_open(path, u'w') as fd:
+				fd.write(self.to_string())
 			self.experiment_path = os.path.dirname(path)
 			return path
 		debug.msg(u'saving as .tar.gz archive (with file pool)')
 		# Write the script to a text file
 		script = self.to_string()
 		script_path = os.path.join(self.pool.folder(), u'script.opensesame')
-		with open(script_path, u'w') as fd:
-			fd.write(safe_str(script))
+		with safe_open(script_path, u'w') as fd:
+			fd.write(script)
 		# Create the archive in a a temporary folder and move it afterwards.
 		# This hack is needed, because tarfile fails on a Unicode path.
 		tmp_path = tempfile.mktemp(suffix=u'.osexp')
@@ -638,8 +638,8 @@ class experiment(item.item):
 		except tarfile.ReadError:
 			# If the file wasn't a .tar.gz, then it must be a plain-text file
 			debug.msg(u"opening plain-text experiment")
-			with open(src, universal_newline_mode) as fd:
-				return safe_decode(fd.read())
+			with safe_open(src, universal_newline_mode) as fd:
+				return fd.read()
 		debug.msg(u"opening .tar.gz archive")
 		# If the file is a .tar.gz archive, extract the pool to the pool folder
 		# and return the contents of opensesame.script.
@@ -667,8 +667,8 @@ class experiment(item.item):
 				os.rmdir(os.path.join(self.pool.folder(), folder))
 		script_path = os.path.join(self.pool.folder(), u"script.opensesame")
 		tar.extract(u"script.opensesame", self.pool.folder())
-		with open(script_path, universal_newline_mode) as fd:
-			script = safe_decode(fd.read())
+		with safe_open(script_path, universal_newline_mode) as fd:
+			script = fd.read()
 		os.remove(script_path)
 		self.experiment_path = os.path.dirname(src)
 		return script

libopensesame/misc.py
@@ -39,7 +39,7 @@ def parse_environment_file():
 	if not os.path.exists(u'environment.yaml'):
 		return
 	import yaml
-	with open(u'environment.yaml') as fd:
+	with safe_open(u'environment.yaml') as fd:
 		d = yaml.load(fd.read())
 	# Convert all values from UTF8 to the filesystem encoding
 	for key, val in d.items():

libopensesame/py3compat.py
@@ -17,7 +17,9 @@ You should have received a copy of the GNU General Public License
 along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import functools
 import sys
+import io
 
 if sys.version_info >= (3,0,0):
 	py3 = True
@@ -63,6 +65,7 @@ def safe_read(path):
 	with open(path, u'r') as fd:
 		return safe_decode(fd.read(), errors=u'ignore')
 
+safe_open = functools.partial(io.open, encoding=u'utf-8')
 
 if py3:
 	safe_str = safe_decode
@@ -70,7 +73,7 @@ else:
 	safe_str = safe_encode
 
 __all__ = ['py3', 'safe_decode', 'safe_encode', 'safe_str',
-	'universal_newline_mode', 'safe_read']
+	'universal_newline_mode', 'safe_read', 'safe_open']
 if not py3:
 	__all__ += ['str', 'bytes']
 else:

libopensesame/python_workspace.py
@@ -119,7 +119,7 @@ class python_workspace(object):
 				type:	str
 		"""
 
-		with open(path) as fd:
+		with safe_open(path) as fd:
 			script = fd.read()
 		bytecode = self._compile(script)
 		self._exec(bytecode)

libqtopensesame/misc/markdown_parser.py
@@ -72,8 +72,8 @@ class markdown_parser(base_subcomponent):
 
 		self.setup(main_window)
 		self.css = u'<style type="text/css">'
-		with open(self.main_window.theme.resource(u'markdown.css')) as fd:
-			self.css += safe_decode(fd.read()) % {u'background_image' : \
+		with safe_open(self.main_window.theme.resource(u'markdown.css')) as fd:
+			self.css += fd.read() % {u'background_image' : \
 				os.path.abspath(self.main_window.theme.resource(
 				u'background.png'))}
 		if highlight is not None:

libqtopensesame/misc/theme.py
@@ -69,7 +69,7 @@ class theme:
 		if os.path.exists(self.theme_info):
 			info = imp.load_source(self.theme,
 				safe_str(self.theme_info, enc=misc.filesystem_encoding()))
-			with open(os.path.join(self.theme_folder, info.qss)) as fd:
+			with safe_open(os.path.join(self.theme_folder, info.qss)) as fd:
 				self._qss = fd.read()
 			self._icon_map = info.icon_map
 			self._icon_theme = info.icon_theme
@@ -236,9 +236,9 @@ class theme:
 		self.icon_map = {}
 		path = os.path.join(self.theme_folder, self._icon_map)
 		debug.msg(path)
-		with open(path) as fd:
+		with safe_open(path) as fd:
 			for l in fd:
-				l = l.split(",")
+				l = l.split(u',')
 				if len(l) == 3:
 					try:
 						size = int(l[2])

libqtopensesame/qtopensesame.py
@@ -215,7 +215,7 @@ class qtopensesame(QtWidgets.QMainWindow, base_component):
 			tmpl = u'default-py3.osexp'
 		else:
 			tmpl = u'default.osexp'
-		with open(misc.resource(os.path.join(u'templates', tmpl)), u'r') as fd:
+		with safe_open(misc.resource(os.path.join(u'templates', tmpl)), u'r') as fd:
 			self.experiment = experiment.experiment(self, u'New experiment',
 				fd.read())
 		self.experiment.build_item_tree()

commit 5f2f5a15928851e753b55fe4cad97cc6544ed396

Don't safe_open() ui files on Python 2 - Works around a peculiarity of PyQt - Fixes a regression introduced in afb5a45

libqtopensesame/misc/base_component.py
@@ -88,8 +88,12 @@ class base_component(base_qtobject):
 					from libopensesame import misc
 					ui_path = misc.resource(os.path.join(*path_list)+u'.ui')
 			debug.msg(u'dynamically loading ui: %s' % ui_path)
-			with safe_open(ui_path) as fd:
-				self.ui = uic.loadUi(fd, self)
+			if py3:
+				with safe_open(ui_path) as fd:
+					self.ui = uic.loadUi(fd, self)
+			else: # On Python 2 we need to open it the old-fashioned way
+				with open(ui_path) as fd:
+					self.ui = uic.loadUi(fd, self)
 		else:
 			self.ui = None
 