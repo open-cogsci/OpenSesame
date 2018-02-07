32ce22d51c6666181a4eb0261f2b8399c8f35862
Daniel Schreij <dschreij@gmail.com>
2018-02-07 16:22:33 +0100

    Remove useRetina parameter from psycho init

--- openexp/_canvas/psycho.py
+++ openexp/_canvas/psycho.py
@@ -124,8 +124,7 @@ class Psycho(Canvas, PsychoCoordinates):
 			units=u'pix',
 			color=Color(experiment, experiment.var.background).backend_color,
 			winType=u'pyglet',
-			allowStencil=True,
-			useRetina=False
+			allowStencil=True
 		)
 		event.Mouse(visible=False, win=experiment.window)
 		experiment.window.winHandle.set_caption(
