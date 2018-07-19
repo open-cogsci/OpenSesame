dd0269d313cbfa093e46cfced569df583c706536
Sebastiaan Mathot <s.mathot@cogsci.nl>
2018-07-19 10:18:03 +0200

    canvas: use shapely to determine whether a point is close enough to a line to be 'inside' it - fall back to bounding box if shapely is not available - related to #631

--- openexp/_canvas/_line/line.py
+++ openexp/_canvas/_line/line.py
@@ -23,13 +23,40 @@ from openexp._canvas._element.element import Element
 
 class Line(Element):
 
+	CONTAINS_MAX_DIST = 10
+
 	def __init__(self, canvas, sx, sy, ex, ey, **properties):
 
 		properties = properties.copy()
 		properties.update({'sx' : sx, 'sy' : sy, 'ex' : ex, 'ey' : ey})
+		self._shapely_line = None
 		Element.__init__(self, canvas, **properties)
 
 	@property
 	def rect(self):
 
 		return self._rect(self.sx, self.sy, self.ex-self.sx, self.ey-self.sy)
+
+	def __contains__(self, xy):
+
+		# Shapely is used to determine whether a point is close enough to the
+		# line to be counted as 'inside' it. If shapely isn't available, we fall
+		# back to a simple bounding box. The shapely LineString is stored for
+		# performance.
+		try:
+			from shapely.geometry import LineString, Point
+		except ImportError:
+			return Element.__contains__(self, xy)
+		if self._shapely_line is None:
+			self._shapely_line = LineString([
+				(self.sx, self.sy),
+				(self.ex, self.ey)
+			])
+		return Point(*xy).distance(self._shapely_line) < self.CONTAINS_MAX_DIST
+
+	def _on_attribute_change(self, **kwargs):
+
+		# When a change occurs the shapely line (if any) needs to be
+		# redetermined in __contains__
+		self._shapely_line = None
+		Element._on_attribute_change(self, **kwargs)
