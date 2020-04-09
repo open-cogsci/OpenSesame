a35cb0e25c4f14254ea8b9a2ef81dccfc09c47cd
Sebastiaan Mathot <s.mathot@cogsci.nl>
2020-04-09 12:34:36 +0200

    psycho: only apply text-positioning fix on Mac OS - other platforms work fine and there may be side-effects

--- openexp/_canvas/_richtext/psycho.py
+++ openexp/_canvas/_richtext/psycho.py
@@ -18,6 +18,7 @@ along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from libopensesame.py3compat import *
+import platform
 from openexp._canvas._richtext.richtext import RichText
 from openexp._canvas._element.psycho import PsychoElement
 from psychopy import visual
@@ -28,18 +29,23 @@ class Psycho(PsychoElement, RichText):
 	def prepare(self):
 
 		im = self._to_pil()
-		# When displaying on Mac retina screens, the resolutions reported by psychopy's window object
-		# and OpenSesame's experiment object may diverge. This results in incorrectly rendered text
-		# with respect to scale and positioning. We correct for the discrepancy between reported sizes
-		# by the ratios calculated below. If the sizes reported by win and experiment are equal, the ratio
-		# values should end up as 1, and nothing changes.
-		x_ratio = int(self.win.size[0] / self.experiment.width)
-		y_ratio = int(self.win.size[1] / self.experiment.height)
-		# Only resize if necessary to prevent unnecessary operations
-		if x_ratio != 1 or y_ratio != 1:
-			im = im.resize((im.width * x_ratio, im.height * y_ratio))
-		x, y = self.to_xy(self.x * x_ratio, self.y * y_ratio)
-		if not self.center:
-			x += im.width // 2
-			y -= im.height // 2
+		if platform.system() == 'Darwin':
+			# When displaying on Mac retina screens, the resolutions reported
+			# by psychopy's window object and OpenSesame's experiment object
+			# may diverge. This results in incorrectly rendered text with
+			# respect to scale and positioning. We correct for the discrepancy
+			# between reported sizes by the ratios calculated below. If the
+			# sizes reported by win and experiment are equal, the ratio values
+			# should end up as 1, and nothing changes.
+			x_ratio = int(self.win.size[0] / self.experiment.width)
+			y_ratio = int(self.win.size[1] / self.experiment.height)
+			# Only resize if necessary to prevent unnecessary operations
+			if x_ratio != 1 or y_ratio != 1:
+				im = im.resize((im.width * x_ratio, im.height * y_ratio))
+			x, y = self.to_xy(self.x * x_ratio, self.y * y_ratio)
+			if not self.center:
+				x += im.width // 2
+				y -= im.height // 2
+		else:
+			x, y = self.to_xy(self.x, self.y)
 		self._stim = visual.SimpleImageStim(self.win, im, pos=(x, y))
