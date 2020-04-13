4ac48e9d6111b0a2924dd23bb3a24fe2dcc10ac7
Sebastiaan Mathot <s.mathot@cogsci.nl>
2020-04-13 19:32:33 +0200

    toolbar_menu: avoid hiding both menu and toolbar

--- opensesame_extensions/toolbar_menu/toolbar_menu.py
+++ opensesame_extensions/toolbar_menu/toolbar_menu.py
@@ -18,12 +18,13 @@ along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from libopensesame.py3compat import *
-
 from qtpy import QtCore, QtWidgets
 from libqtopensesame.misc.config import cfg
+from libopensesame.oslogging import oslogger
 from libqtopensesame.extensions import base_extension
 from libqtopensesame.misc.translate import translation_context
 _ = translation_context(u'toolbar_menu', category=u'extension')
+
 
 class toolbar_menu(base_extension):
 
@@ -34,31 +35,12 @@ class toolbar_menu(base_extension):
 
 	def event_startup(self):
 
-		"""
-		desc:
-			On startup, a widget is created with a menu that copies all actions
-			from the main menu bar.
-		"""
-
-		self.menu = QtWidgets.QMenu()
-		for action in self.menubar.actions():
-			self.menu.addAction(action)
-		self.stretch = QtWidgets.QWidget()
-		self.stretch.setSizePolicy(QtWidgets.QSizePolicy.Expanding,
-			QtWidgets.QSizePolicy.Expanding)
-		self.button = QtWidgets.QPushButton(self.theme.qicon(self.icon()),
-			_(u'Menu'))
-		self.button.setMenu(self.menu)
-		self.button.setIconSize(QtCore.QSize(cfg.toolbar_size,
-			cfg.toolbar_size))
-		self.button.setFlat(True)
-		self.toolbar.addWidget(self.stretch)
-		self.menu_action = self.toolbar.addWidget(self.button)
-		if cfg.toolbar_menu_active:
-			self.activate_toolbar_menu()
-			self.set_checked(True)
-		else:
-			self.deactivate_toolbar_menu()
+		self._menu = None
+		if not cfg.toolbar_menu_active:
+			return
+		cfg.toolbar_menu_active = False  # It will be toggled!
+		self.activate()
+		self.set_checked(True)
 
 	def activate(self):
 
@@ -67,12 +49,49 @@ class toolbar_menu(base_extension):
 			Toggle the menubar integration.
 		"""
 
+		if self._menu is None:
+			self._init_menu()
 		if cfg.toolbar_menu_active:
-			self.deactivate_toolbar_menu()
+			self._deactivate_toolbar_menu()
 		else:
-			self.activate_toolbar_menu()
+			self._activate_toolbar_menu()
+			
+	def _init_menu(self):
+		
+		"""
+		desc:
+			Creates a widget with a menu that copies all actions from the main
+			menu bar.
+		"""		
+			
+		self._menu = QtWidgets.QMenu()
+		for action in self.menubar.actions():
+			self._menu.addAction(action)
+		self.stretch = QtWidgets.QWidget()
+		self.stretch.setSizePolicy(
+			QtWidgets.QSizePolicy.Expanding,
+			QtWidgets.QSizePolicy.Expanding
+		)
+		self.button = QtWidgets.QPushButton(
+			self.theme.qicon(self.icon()),
+			_(u'Menu')
+		)
+		self.button.setMenu(self._menu)
+		self.button.setIconSize(
+			QtCore.QSize(cfg.toolbar_size, cfg.toolbar_size)
+		)
+		self.button.setFlat(True)
+		self.toolbar.addWidget(self.stretch)
+		self.menu_action = self.toolbar.addWidget(self.button)
+			
+	def _keep_toolbar_visible(self, visible):
+		
+		if visible:
+			return
+		oslogger.debug('keep toolbar visible')
+		self.toolbar.setVisible(True)
 
-	def activate_toolbar_menu(self):
+	def _activate_toolbar_menu(self):
 
 		"""
 		desc:
@@ -83,8 +102,11 @@ class toolbar_menu(base_extension):
 		self.menubar.setVisible(False)
 		self.menu_action.setVisible(True)
 		self.stretch.setVisible(True)
+		if not self.toolbar.isVisible():
+			self.toolbar.setVisible(True)
+		self.toolbar.visibilityChanged.connect(self._keep_toolbar_visible)
 
-	def deactivate_toolbar_menu(self):
+	def _deactivate_toolbar_menu(self):
 
 		"""
 		desc:
@@ -95,3 +117,4 @@ class toolbar_menu(base_extension):
 		self.menubar.setVisible(True)
 		self.menu_action.setVisible(False)
 		self.stretch.setVisible(False)
+		self.toolbar.visibilityChanged.disconnect()
