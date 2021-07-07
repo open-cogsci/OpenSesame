open-cogsci/OpenSesame

commit bce32e9dbbbe2dba2f0de0a6ce5c955f4036f4c5

syntax: quote single words that contain non-ascii characters - fixes open-cogsci/osweb#67

libopensesame/syntax.py
@@ -494,7 +494,7 @@ class syntax(object):
 			float(s)
 			assert(s == s.strip())  # No flanking whitespace
 		except:
-			if not s.replace(u'_', u'').isalnum():
+			if not s.replace(u'_', u'').isascii():
 				return u'"%s"' % s.replace(u'"', u'\\"')
 		return s
 

commit 3787defe47bacdf2d7632a90576ceccc68a8dea4

fix regression introduced in bce32e9dbbbe2dba2f0de0a6ce5c955f4036f4c5 - quote values if they are neither alphanumeric nor ascii

libopensesame/syntax.py
@@ -494,7 +494,8 @@ class syntax(object):
 			float(s)
 			assert(s == s.strip())  # No flanking whitespace
 		except:
-			if not s.replace(u'_', u'').isascii():
+			t = s.replace(u'_', u'')
+			if not t.isalnum() or not t.isascii():
 				return u'"%s"' % s.replace(u'"', u'\\"')
 		return s
 