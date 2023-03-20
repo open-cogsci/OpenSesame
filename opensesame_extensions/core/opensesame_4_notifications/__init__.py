0c196c6b3086de5de8de2e51ac5c52cff54d4b1f
smathot <s.mathot@cogsci.nl>
2023-03-20 13:53:00 +0100

    Revert removal of opensesame_4_notifications - We need to retain the old experiment message

---
+++ opensesame_extensions/core/opensesame_4_notifications/__init__.py
@@ -0,0 +1,9 @@
+"""Provides tips for new users of OpenSesame 4"""
+
+icon = "applications-accessories"
+label = "OpenSesame 3 notifications"
+priority = -1
+settings = {
+    "os4n_new_user_notification": True,
+    "os4n_old_experiment_notification": True
+}
