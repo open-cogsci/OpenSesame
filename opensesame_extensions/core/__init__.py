38f9ac02ae8311d895b6027e4c81bd31ba367e6c
smathot <s.mathot@cogsci.nl>
2023-03-20 13:52:46 +0100

    Update package names for updater

--- opensesame_extensions/core/__init__.py
+++ opensesame_extensions/core/__init__.py
@@ -3,6 +3,7 @@
 from libopensesame.metadata import __version__
 
 # The name of the packages to check for updates on conda and pip
-packages = ['opensesame-core', 'datamatrix', 'qdatamatrix',
+packages = ['opensesame', 'opensesame-core', 'datamatrix', 'qdatamatrix',
             'python-pseudorandom', 'python-qnotifications',
-            'python-fileinspector', 'pyqode.python', 'pyqode.core']
+            'python-fileinspector', 'pyqode.python', 'pyqode.core',
+            'pyqode3-python', 'pyqode3-core']
